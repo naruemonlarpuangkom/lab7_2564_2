2876ed79b2d165981ec1054f24b0c6e11a1c0e81
naruemonlarpuangkom <narumon.02545@gmail.com>
2021-11-30 16:46:20 +0700

    lab7_2index

--- lab7_2.cpp
+++ lab7_2.cpp
@@ -9,6 +9,27 @@ int main(){
 		P[i] = 2*i+1;
 		i++;
 	}
+	cout << "Please input k: ";
+	cin >> k;
+	if(k > 0){
+		while (i < 5000){
+			if((P[i]%k) == 0){
+				result = result -P[i];
+			}
+			else{
+				result = result +P[i];
+			}
+			i++;
+			}
+			cout << result;
+		{
+		i++;
+		}
+		cout << "Result = " << result;
+	}
+	else{
+		cout << "Invalid input!!!" ;
+	}
 	
 	return 0;
 }
