efa1b5330723227572e55e26117012eebc790315
naruemonlarpuangkom <narumon.02545@gmail.com>
2021-11-30 18:11:18 +0700

    lab7_4

--- lab7_4.cpp
+++ lab7_4.cpp
@@ -9,7 +9,12 @@ int main(){
 	int id[] = {123,196,225,329,420,451,515,666,690,787};
 	double gpa[] = {1.50,2.12,4.00,3.69,2.50,2.76,3.54,2.14,3.50,1.88};
 
-    //Write your code here.
-		
+	int i = 10;
+	while (i = 10){
+		cout << "Luffy","Zoro","Sanji","Nami","Naruto","Sakura","All Might","Saitama","Eren","Mikasa";
+		cin >> i;
+	}
+	
+
 	return 0;
 }
